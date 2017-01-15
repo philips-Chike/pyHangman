6b4d729738533988e39e6af461dc789e5185123b
Evilizard83 <d.maheux@gmail.com>
2017-01-15 00:26:46 -0500

    update art.py
    Previously, art.py read the artwork from 7 different files..
    Now it's just a function creating 7 different drawings :-).

--- art.py
+++ art.py
@@ -1,6 +1,18 @@
-def drawhangman(graphic):
-    """This function receives the filename as a param. prints it"""
-    with open(graphic,'r') as hangman:
-        drawit = hangman.readlines()
-    for i in drawit:
-        print(i.strip('\n'))
+def drawhangman(err=0):
+    """This function prints the hangman corresponding to the error amount"""
+    if err == 0 :  hman = ("  |       ","  |       ","  |       ")
+    elif err == 1: hman = ("  |    O  ","  |       ","  |       ")
+    elif err == 2: hman = ("  |    O  ","  |    |  ","  |       ")
+    elif err == 3: hman = ("  |    O  ","  |   /|  ","  |       ")
+    elif err == 4: hman = ("  |    O  ","  |   /|\\","  |       ")
+    elif err == 5: hman = ("  |    O  ","  |   /|\\","  |   /   ")
+    elif err == 6: hman = ("  |    O  ","  |   /|\\","  |   / \\")
+    top =   ("  ______  ","  |    |  ")
+    bottom =("  |       ","__|____   ","|      |___","|_________|")
+    
+    for i in top:
+        print(i)
+    for i in hman:
+        print(i)
+    for i in bottom:
+        print(i)
